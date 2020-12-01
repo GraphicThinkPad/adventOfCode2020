050d158cb771cca247cd0b68fe7cec47f381ef7d
Kory Smith <kory.smith@kroger.com>
2020-12-01 11:42:55 -0500

    Solve problem 1 part 2

--- 1/1.js
+++ 1/1.js
@@ -4,12 +4,19 @@ function twoNumbersSumTo2020(x, y) {
   return x + y === 2020;
 }
 
+function threeNumbersSumTo2020(x, y, z) {
+  return x + y + z === 2020;
+}
+
 for (let i = 0; i <= input.length - 1; i++) {
-  const current = input[i];
-  for (let j = 0; j <= input.length - 1; j++) {
-    const comparator = input[j];
-    if (twoNumbersSumTo2020(current, comparator)) {
-      console.log(current * comparator);
+  const first = input[i];
+  for (let j = 1; j <= input.length - 1; j++) {
+    const second = input[j];
+    for (let z = 2; z <= input.length - 1; z++) {
+      const third = input[z];
+      if (threeNumbersSumTo2020(first, second, third)) {
+        console.log(first * second * third);
+      }
     }
   }
 }
